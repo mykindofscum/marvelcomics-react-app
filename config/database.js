4a00ae8d33ebc9f877bf17c651bf20cf9e2ccf93
mykindofscum <alvinkchan99@gmail.com>
2019-07-10 08:38:21 -0700

    Add db and env routes to server.js and pull info from api tester

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = reuire('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser:
+true })
+
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`DB connected to ${process.env.DATABASE_URL}`);
+})
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+require('dotenv').config();
+require('./config/database');
 
 const app = express();
 
@@ -11,6 +13,16 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Put your API routes here, before the "catch all"
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
+
+  const port = process.env.PORT || 3001;
+
+  app.listen(port, function() {
+      console.log(`App running on port $(port)`);
+  });
+
+  
