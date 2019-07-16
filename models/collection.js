e9cb68e325486dc01f404c93149e75e40e7a19ea
mykindofscum <alvinkchan99@gmail.com>
2019-07-15 21:33:36 -0700

    Add user and comic to schema

--- models/collection.js
+++ models/collection.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const collectionSchema = new Schema({
+    id: Number,
     title: String,
     issueNumber: Number,
     date: Number,
     pageCount: Number,
     description: String,
-    creators: String
+    creators: String,
+    comic: [comicSchema],
+    user: [userSchema]
 });
+
+
 
 module.exports = mongoose.model('Collection', collectionSchema);
 
