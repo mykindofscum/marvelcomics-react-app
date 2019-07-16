116024aa076c0bc77e086aa71b3ca2236dcf7e71
mykindofscum <alvinkchan99@gmail.com>
2019-07-15 17:09:14 -0700

    Setup refactor SearchResults.jsx

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,31 +1,28 @@
-import React, { Component} from 'react';
+import React from 'react';
 // import { Link } from 'react-router.dom';
 import './SearchResults.css';
 
+const SearchResults = (props) => {
+    // const comicData = this.props.result.results
+    // const {thumbnail, issueNumber, pageCount, dates, prices} = comicData;
+    let results = '';
+    console.log(props.result.results);
+    if (props.result.results) {
+        results = props.result.results.map((result) => (
+        <div>
+            <p>title: {result.title}</p>
+            {/* <p> {result.thumbnail} </p> */}
+        </div>
 
-class SearchResults extends Component {
-    render() {
-        const Data = this.props.comic;
-        const {thumbnail, issueNumber, pageCount, dates, prices} = Data;
-            return (
-                <div className="result">
-                    <div className="result-thumbnail-container">
-                        <img className="result-thumbnail" src={`${thumbnail.path}.${thumbnail.extension}`} alt="thumbnail" />
-                    </div>
-                    <div className="result-data-container">
-                        <h1>{Data.title}</h1>
-                        <p>Issue: {issueNumber}</p>
-                        <p>Pages: {pageCount}</p>
-                        <p>Date: {new Date(dates[0].date.toLocaleDateString())}</p>
-                        <p>Price: ${prices[0].price}</p>
-                    </div>
-                    {/* <Link to='/'>RETURN</Link> */}
-                </div>
+    ))
+}
+    console.log(results);
+        return (
+            <div>
+                something
+                {results}
+            </div>
         )
     }
-}
-
 export default SearchResults;
-
-// {this.props.results.map((comic, index) => <SearchResults comic={comic} key={index} />)}
 
