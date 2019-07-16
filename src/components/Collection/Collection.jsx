f8d66cb5977ded332ee68580ac70239a0cdfae59
mykindofscum <alvinkchan99@gmail.com>
2019-07-16 16:07:42 -0700

    Setup up collection page

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Collection.css';
+// import comicService from '../../utils/comicService';
 
-class Collection extends Component {             
-    render() {
-        
-        return (
-            <div className='NavBar'>
-                <h3>Collection</h3>
-            </div>
-        );
+const Collection = (props) => {             
+    let collection = [];
+    if (props.collection) {
+        collection = props.collection.map((collection) => (
+            <div className="comic">
+                <div className="comic-thumbnail-container">
+                
+                    <img className="comic-thumbnail" 
+                    src={`${collection.thumbnail.path}.${collection.thumbnail.extension}`} 
+                    alt="thumbnail" />
+                </div>
+                <div className="comic-details">
+                    <h5>{collection.title}</h5>
+                    <p >Description:</p>
+                    <p>Issue: {collection}.issueNumber}</p>
+                    <p>Pages: {collection.pageCount}</p>
+                    <p>Price: ${collection.prices[0].price}</p>
+                    {/* <button onClick={(e) => props.handleAddComic(e, result)}>ADD</button> */}
+                </div>
+            </div>        
+        ))
     }
+
+return (
+    <div className="collection">
+            <h3>Collection</h3>
+        <section>
+            { collection }
+        </section>
+    </div>
+    )
 }
 export default Collection;
+        
