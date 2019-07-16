0241db1c9d2612ef6bf30f017f80270e9285bb16
mykindofscum <alvinkchan99@gmail.com>
2019-07-16 16:03:36 -0700

    Rename addComic to addCollection

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -2,7 +2,10 @@ let express = require('express');
 let router = express.Router();
 let collectionCtrl = require('../../controllers/collection');
 
+
 router.get('/api/comics', collectionCtrl.getCollection);
-router.post('/api/comics', collectionCtrl.addComic);
+router.post('/api/comics', collectionCtrl.addCollection);
+
+
 
 module.exports = router;
