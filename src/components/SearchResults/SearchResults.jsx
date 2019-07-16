9831a3a9360865cdd2fd823942f0144abb721727
mykindofscum <alvinkchan99@gmail.com>
2019-07-15 19:10:20 -0700

    Add img link and comic details

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -3,26 +3,39 @@ import React from 'react';
 import './SearchResults.css';
 
 const SearchResults = (props) => {
-    // const comicData = this.props.result.results
-    // const {thumbnail, issueNumber, pageCount, dates, prices} = comicData;
+    // const comicData = props.result.results;
+    // const {thumbnail} = comicData;
     let results = '';
     console.log(props.result.results);
     if (props.result.results) {
         results = props.result.results.map((result) => (
-        <div>
-            <p>title: {result.title}</p>
-            {/* <p> {result.thumbnail} </p> */}
-        </div>
-
+            <div className="comic">
+                <div className="comic-thumbnail-container">
+                    <img className="comic-thumbnail" src={`${result.thumbnail.path}.${result.thumbnail.extension}`} alt="thumbnails" />
+                </div>
+                <div className="comic-details">
+                        <h2>Title: {result.title}</h2>
+                        <p>Description:</p>
+                        <p>Issue: {result.issueNumber}</p>
+                        <p>Pages: {result.pageCount}</p>
+                        <p>Date: {result.date}</p>
+                        <p>Price: ${result.prices[0].price}</p>
+                </div>
+            </div>
     ))
 }
     console.log(results);
         return (
             <div>
-                something
-                {results}
+                <section>
+                    {results}
+                </section>
             </div>
         )
     }
 export default SearchResults;
+                    
+                    
+
+// , issueNumber, pageCount, dates, prices
 
