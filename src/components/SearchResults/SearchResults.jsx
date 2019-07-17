b7b40be53e9108d5858af5a71d655931855d5bc7
mykindofscum <alvinkchan99@gmail.com>
2019-07-16 22:22:37 -0700

    styled thumbnails, title, description and add class to description

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -3,16 +3,15 @@ import './SearchResults.css';
 
 const SearchResults = (props) => {
     let results = '';
-    console.log(props.result.results);
     if (props.result.results) {
         results = props.result.results.map((result) => (
             <div className="comic">
                 <div className="comic-thumbnail-container">
-                    <img className="comic-thumbnail" src={`${result.thumbnail.path}.${result.thumbnail.extension}`} alt="thumbnails" />
+                    <img className="comic-thumbnail zoom zoom:hover" src={`${result.thumbnail.path}.${result.thumbnail.extension}`} alt="thumbnails" />
                 </div>
                 <div className="comic-details">
-                    <h5>{result.title}</h5>
-                    <p >Description:</p>
+                    <h6>{result.title}</h6>
+                    <p className="description">Description: {result.description}</p>
                     <p>Issue: {result.issueNumber}</p>
                     <p>Pages: {result.pageCount}</p>
                     <p>Price: ${result.prices[0].price}</p>
