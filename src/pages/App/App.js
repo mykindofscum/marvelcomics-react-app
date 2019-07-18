edd307b72b79efeb0f7270dc1d5a000e57bccdf3
mykindofscum <alvinkchan99@gmail.com>
2019-07-18 09:53:46 -0700

    Edit app.js import SignupPage to jsx

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link, Redirect } from 'react-router-dom';
 import './App.css';
-import SignupPage from '../SignupPage/SignupPage';
+import SignupPage from '../SignupPage/SignupPage.jsx';
 import LoginPage from '../LoginPage/LoginPage';
 import userService from '../../utils/userService';
 import comicService from '../../utils/comicService';
