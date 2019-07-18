9c2542b66b35e686c40b81ec57353cfa4ab3ddf9
mykindofscum <alvinkchan99@gmail.com>
2019-07-18 11:49:57 -0700

    Edit imports in SignupPage

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
 import SignupForm from '../../components/SignupForm/SignupForm';
 import './SignupPage.css';
 
