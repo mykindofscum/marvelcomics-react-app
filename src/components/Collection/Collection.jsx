da04b34a729c972558bff47c4aefe067cb1a59d3
mykindofscum <alvinkchan99@gmail.com>
2019-07-26 14:27:58 -0700

    Remove if statement

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+// import React from 'react';
 import './Collection.css';
 
 const Collection = (props) => {             
-    let collection = [];
-    if (props.collection) {
-        collection = props.collection.map((collection) => (
+    // let collection = [];
+    // if (props.collection) {
+        console.log("porp",props.collection);
+       let collection = props.collection.map((collection) => (
+
             <div className="comic">
                 <div className="comic-thumbnail-container">
-                
-                    {/* <img className="comic-thumbnail" 
-                    src={`${collection.thumbnail.path}.${collection.thumbnail.extension}`} 
+                     {/* <img className="comic-thumbnail" 
+                     src={`${collection.thumbnail.path}.${collection.thumbnail.extension}`} 
                     alt="thumbnail" /> */}
                 </div>
                 <div className="comic-details">
                     <h5>{collection.title}</h5>
                     <p >Description:</p>
-                    <p>Issue: {props.issueNumber}</p>
-                    <p>Pages: {props.pageCount}</p>
-                    <p>Price: ${props.price}</p>
+                    <p>Issue: {collection.issueNumber}</p>
+                    <p>Pages: {collection.pageCount}</p>
+                    <p>Price: ${collection.price}</p>
+                    <button onClick={(e)=>props.handleDelComic(e, collection) }>DELETE</button>
                 </div>
             </div>        
         ))
-    }
+    // }
 
     return (
         <div className="collection">
